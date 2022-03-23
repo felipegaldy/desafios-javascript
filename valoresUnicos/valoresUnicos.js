b4625e288cbdef6a7e592a930ac46732440eae74
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-03-23 12:17:12 -0300

    alteração na solução, agora com spread

--- valoresUnicos/valoresUnicos.js
+++ valoresUnicos/valoresUnicos.js
@@ -1,5 +1,9 @@
 /* Dado o array `[30, 30, 40, 5, 223, 2049, 3034, 5]`, retorne outro array apenas com valores únicos. */
 var array = [30, 30, 40, 5, 223, 2049, 3034, 5]
-var mySet = new Set(array)
-console.log(mySet)
-console.log(array)
+
+function valorUnico(array){
+    const mySet = new Set(array)
+
+    return [...mySet]
+}
+console.log(valorUnico(array))
