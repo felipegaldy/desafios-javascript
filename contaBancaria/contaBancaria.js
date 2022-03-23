7ab745e7225c65c63272ea7bd176e76da5090af8
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-03-23 18:44:29 -0300

    desafio conta bancaria orientação objetos

---
+++ contaBancaria/contaBancaria.js
@@ -0,0 +1,64 @@
+class contaBancaria {
+    constructor (agencia, numero, tipo, saldo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+
+    get saldo() {
+        return this._saldo
+    }
+    set saldo(valor){
+        this._saldo = valor; 
+    }
+//3. Dentro de `ContaBancaria`, crie os métodos `sacar` e `depositar`;
+    sacar(valor){
+        if(this.valor > this._saldo) throw RangeError('Saldo Insuficiente');
+
+        this._saldo -= valor
+        return this._saldo; 
+    }
+    depositar(valor) {
+        this._saldo += valor
+        
+        return this._saldo
+    }
+}
+
+class contaCorrente extends contaBancaria {
+    
+    constructor(agencia, numero, saldo, cartaoCredito){
+    super(agencia, numero, saldo);
+    this.cartaoCredito = cartaoCredito
+    this.tipo = 'corrente';
+    }
+    get cartaoCredito() {
+        return this.cartaoCredito
+    }
+    set cartaoCredito(valor){
+        return this.cartaoCredito = valor
+    }
+}
+
+class contaPoupanca extends contaBancaria {
+    constructor(agencia, numero, saldo){
+        super(agencia, numero, saldo);
+        this.tipo = 'poupanca'
+    }
+
+}
+
+class contaUniversitaria extends contaBancaria {
+    constructor(agencia, numero, saldo){
+        super(agencia, numero, saldo);
+        this.tipo = 'universitaria'
+    }
+    sacar(valor) {
+        if(valor > 500) {
+            return 'Solicite um valor menor que 500R$'
+        }
+        this._saldo -= valor
+        return this._saldo
+    }
+}
