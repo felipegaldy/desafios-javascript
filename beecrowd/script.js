1f667dffa5baaa303f4e8c3a06c3c9b504039817
felipegaldy <felipesilveira4@fatec.sp.gov.br>
2022-04-28 18:21:09 -0300

    treino com regexp

--- beecrowd/script.js
+++ beecrowd/script.js
@@ -1,32 +1,10 @@
 var input = require('fs').readFileSync('stdin','utf8');
-var lines = input.split(' ').map(Number);
+var lines = input.split('\n')
 
-lines.sort((a,b) => b-a);
-
-var [A, B, C] = lines;
-var A = parseFloat(A);
-var B = parseFloat(B);
-var C = parseFloat(C);
-
-if(A >= (B+C)){
-    console.log('NAO FORMA TRIANGULO')
-} else {
-    tiposTriangulo();
-}
-
-function tiposTriangulo() {
-    if(A*A == (B**2+C**2)){
-        console.log('TRIANGULO RETANGULO')
-    }
-    if(A*A > (B**2+C**2)){
-        console.log('TRIANGULO OBTUSANGULO')
-    }
-    if(A*A < (B**2+C**2)){
-        console.log('TRIANGULO ACUTANGULO')
-    }
-    if(A == B && A == C){
-        console.log('TRIANGULO EQUILATERO')
-    }else if(A == B || A == C || B == C){
-        console.log('TRIANGULO ISOSCELES')
-    }  
+const str = 'hallo d   é world';
+let regex = /[á-ùÁ-ÙA-Z@!#$%^&*?;|§ªº°~´`¬><"'+=()/\\]/g.test(str);
+if(regex){
+    console.log('mensagem inválida')
+}else {
+    console.log('mensagem valida')
 }
