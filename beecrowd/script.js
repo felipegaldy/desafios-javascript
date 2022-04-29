c6bcc5b0bf19ac4b329777d01e8328b8a085f987
felipegaldy <felipesilveira4@fatec.sp.gov.br>
2022-04-29 09:44:05 -0300

    inicio de solução bee tempo de jogo

--- beecrowd/script.js
+++ beecrowd/script.js
@@ -1,10 +1,3 @@
 var input = require('fs').readFileSync('stdin','utf8');
-var lines = input.split('\n')
+var [inicio, fim] = input.split(' ');
 
-const str = 'hallo d   é world';
-let regex = /[á-ùÁ-ÙA-Z@!#$%^&*?;|§ªº°~´`¬><"'+=()/\\]/g.test(str);
-if(regex){
-    console.log('mensagem inválida')
-}else {
-    console.log('mensagem valida')
-}
